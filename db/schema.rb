6f4daa9a61d067235e188394c257cd667008fe59
Petar Risteski <bobpetar@gmail.com>
2016-04-07 21:01:38 +0000

    categories to new article form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404184110) do
+ActiveRecord::Schema.define(version: 20160407194408) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
